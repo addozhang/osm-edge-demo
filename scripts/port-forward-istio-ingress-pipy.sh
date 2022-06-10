2e4f8451d720e3a9f00e7f4397bf9246ab746d65
cybwan <baili@flomesh.io>
2022-06-10 10:23:16 +0800

    fix

--- scripts/port-forward-istio-ingress-pipy.sh
+++ scripts/port-forward-istio-ingress-pipy.sh
@@ -6,9 +6,10 @@ source .env
 LOCAL_PORT="${LOCAL_PORT:-80}"
 PIPY_INGRESS_SERVICE=${PIPY_INGRESS_SERVICE:-ingress-pipy-controller}
 INGRESS_PIPY_NAMESPACE="${INGRESS_PIPY_NAMESPACE:-flomesh}"
+TEST_NAMESPACE="${TEST_NAMESPACE:-sft-test-flomesh}"
 
 PIPY_INGRESS_PORT="$(kubectl -n "$INGRESS_PIPY_NAMESPACE" get service "$PIPY_INGRESS_SERVICE" -o jsonpath='{.spec.ports[?(@.name=="http")].port}')"
 
-kubectl describe ingress -n "$INGRESS_PIPY_NAMESPACE" pipy-ingress
+kubectl describe ingress -n "$TEST_NAMESPACE" pipy-ingress
 
-kubectl port-forward --address 0.0.0.0 -n $INGRESS_PIPY_NAMESPACE service/$PIPY_INGRESS_SERVICE "$LOCAL_PORT":"$PIPY_INGRESS_PORT" &
+kubectl port-forward --address 0.0.0.0 -n $INGRESS_PIPY_NAMESPACE service/$PIPY_INGRESS_SERVICE "$LOCAL_PORT":"$PIPY_INGRESS_PORT"
